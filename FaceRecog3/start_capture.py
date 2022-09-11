4e08ff6d452f4473dd54d36a7688b200fab47910
Aleksey <rusal@bk.ru>
2022-09-11 20:57:41 +0300

    fix folder structure

--- FaceRecog3/start_capture.py
+++
@@ -1,8 +0,0 @@
-from subprocess import Popen
-
-while True:
-    filename="capture_pg.py"
-    print("\nStarting " + filename)
-    p1 = Popen("python " + filename, shell=True)
-    # p2 = Popen("python " + filename, shell=True)
-    p1.wait()
