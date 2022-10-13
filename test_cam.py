56955a8034b36442b3fcc03400a3bcaa64344599
Aleksey <rusal@bk.ru>
2022-10-13 22:34:05 +0300

    test cam

---
+++ test_cam.py
@@ -0,0 +1,12 @@
+import cv2
+
+vid = cv2.VideoCapture(1)
+while (True):
+    ret, frame = vid.read()
+    # cv2.imshow('frame', frame)
+    cv2.imwrite("./img.png", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
